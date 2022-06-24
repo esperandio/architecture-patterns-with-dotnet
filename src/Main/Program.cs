912e1bec7423ba83bdafad0559cf7db298e9b29c
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-23 22:33:12 -0300

    feat: adicao da extensao de servicos de persistencia no builder da aplicacao (incluindo rota para teste de conexao com banco)

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,6 +1,22 @@
+using Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Get environment
+string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+// Build config
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+builder.Services.AddPersistenceServices(config);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/batches/count", (ApplicationDbContext applicationDbContext) => applicationDbContext.Batches.Count());
 
 app.Run();
