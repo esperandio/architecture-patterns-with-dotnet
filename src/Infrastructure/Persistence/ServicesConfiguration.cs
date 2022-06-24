506218eec769b38390555fbe37416f2b682db36c
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-23 22:31:49 -0300

    feat: criacao de extensao do IServiceCollection para configuracao de servicos referentes a camada de persistencia da aplicacao

---
+++ src/Infrastructure/Persistence/ServicesConfiguration.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence;
+
+public static class ServicesConfiguration
+{
+	public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+	{
+        var connectionString = configuration.GetConnectionString(nameof(ApplicationDbContext));
+
+		services.AddDbContext<ApplicationDbContext>(
+            options => options.UseMySql(
+                connectionString, 
+                new MySqlServerVersion(new Version(8, 0, 29))
+            )
+        );
+	}
+}
