1f4255b2551d3129f996d45b53499d3136cbba20
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2023-04-01 15:31:47 +0000

    feat: criacao da camada Web (entrypoint)

---
+++ src/Web/Program.cs
@@ -0,0 +1,29 @@
+using UseCases;
+using Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureDataService();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/allocate", async (AppDbContext dbContext , AllocateData request) => {
+    try
+    {
+        var repository = new BatchRepository(dbContext);
+
+        var reference = await new AllocateUseCase(repository).Perform(request);
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok(reference);
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.Run();
