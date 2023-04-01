c7e7cce727034a6b02b25a624d0eb848b81c82e5
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2023-04-01 01:24:13 +0000

    feat: criacao de metodo de extensao em Infrastructure.Data para configurar a injecao de dependencia no IServiceCollection para os itens relacionados a persistencia de dados

---
+++ src/Infrastructure.Data/ServicesConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using Domain;
+
+namespace Infrastructure.Data;
+
+public static class ServicesConfiguration
+{
+	public static void AddInfrastructureDataService(this IServiceCollection services)
+	{
+		services.AddDbContext<AppDbContext>();
+
+        services.AddScoped<IBatchRepository, BatchRepository>();
+	}
+}
