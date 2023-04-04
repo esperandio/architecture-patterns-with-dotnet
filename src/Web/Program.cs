8e895ed7bc8d17f105624da92a86711f760a7d22
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2023-04-04 00:38:32 +0000

    feat: implementacao de endpoint para AddBatchUseCase

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -9,12 +9,29 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/allocate", async (AppDbContext dbContext , AllocateData request) => {
+app.MapPost("/allocate", async (AppDbContext dbContext, AllocateData request) => {
     try
     {
         var repository = new BatchRepository(dbContext);
 
         var reference = await new AllocateUseCase(repository).Perform(request);
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok(reference);
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapPost("/batch", async (AppDbContext dbContext, AddBatchData request) => {
+    try
+    {
+        var repository = new BatchRepository(dbContext);
+
+        var reference = await new AddBatchUseCase(repository).Perform(request);
 
         await dbContext.SaveChangesAsync();
 
